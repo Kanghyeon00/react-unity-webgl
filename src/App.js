14d4fd1506a484b010a66aed9281f51b19ba2e9a
Kanghyeon00 <hyeony0419@naver.com>
2023-04-14 14:30:42 +0900

    secon

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import Unity, { UnityContext } from "react-unity-webgl";
+
 
 function App() {
+
+  const unityContext = new UnityContext({
+    loaderUrl: "../Build/Build.loader.js",
+    dataUrl: "../Build/Build.data",
+    frameworkUrl: "../Build/Build.framework.js",
+    codeUrl: "../Build/Build.wasm",
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p style={{fontSize : '24px'}}>Test</p>
+      <Unity style={{
+        width : '80%',
+        height : '100%',
+      }} unityContext={unityContext} />;
     </div>
   );
 }
